6b404932a4bc32cb5abfc60fb390f9c5674c018f
Guy Peled <guy.p@taboola.com>
2019-07-18 21:10:54 +0300

    petitparser_extras

--- lib/src/printers/graphql.dart
+++ lib/src/printers/graphql.dart
@@ -3,55 +3,60 @@
 
 import 'package:petitparser_extras/src/ast/index.dart';
 
-class GraphQLPrinter extends PrinterBase<_GraphQLPrinterContext> {
+class GraphQLPrinter extends PrinterBase<GraphQLPrinterContext> {
 
 
-  void visitCompilationUnit(CompilationUnit compilationUnit, _GraphQLPrinterContext context) {
+  void visitCompilationUnit(CompilationUnit compilationUnit, GraphQLPrinterContext context) {
     print_list(compilationUnit.children, context.OperationsStyle, context);
   }
 
-  void visitArgumentNode(ArgumentNode argumentNode, _GraphQLPrinterContext context) {
+  void visitArgumentNode(ArgumentNode argumentNode, GraphQLPrinterContext context) {
     print_item(argumentNode.name, null, context);
     print_item(argumentNode.value, context.ArgumentValueStyle, context);
   }
 
-  void visitFieldNode(FieldNode fieldNode, _GraphQLPrinterContext context) {
+  void visitFieldNode(FieldNode fieldNode, GraphQLPrinterContext context) {
     print_item(fieldNode.name, fieldNode.arguments?.isEmpty ?? true ? context.SpaceAfterStyle : null, context);
     print_list(fieldNode.arguments, context.FieldArgumentsStyle, context);
     print_list(fieldNode.fields, context.FieldsStyle, context);
   }
 
-  void visitExpressionNode(ExpressionNode valueNode, _GraphQLPrinterContext  context) {
+  void visitExpressionNode(ExpressionNode valueNode, GraphQLPrinterContext  context) {
     this.print_item(valueNode.toString(), null, context);
   }
 
-  void visitOperationNode(OperationNode operationNode, _GraphQLPrinterContext context) {
+  void visitOperationNode(OperationNode operationNode, GraphQLPrinterContext context) {
     this.print_item(operationNode.typeName, context.SpaceAfterStyle, context);
     this.print_item(operationNode.name, context.SpaceAfterStyle, context);
     this.print_list(operationNode.fields, context.FieldsStyle, context);
   }
 
-  void visitVariableNode(VariableNode variableNode, _GraphQLPrinterContext context) {
+  void visitVariableNode(VariableNode variableNode, GraphQLPrinterContext context) {
     this.print_item("\$${variableNode.name}", null, context);
   }
 
-  void visitPrimitiveNode(PrimitiveNode primitiveNode, _GraphQLPrinterContext context) {
+  void visitPrimitiveNode(PrimitiveNode primitiveNode, GraphQLPrinterContext context) {
     this.print_item(primitiveNode.toString(), null, context);
   }
 
   @override
-  _GraphQLPrinterContext createContext(bool indentation) {
+  GraphQLPrinterContext createContext(bool indentation) {
     if(indentation) {
       return _GraphQLPrinterIndentedContext();
     } else {
-      return _GraphQLPrinterContext();
+      return _GraphQLPrinterCompactContext();
     }
   }
-
-
 }
 
-class _GraphQLPrinterIndentedContext extends PrintContext implements _GraphQLPrinterContext {
+abstract class GraphQLPrinterContext implements PrintContext {
+  PrintListStyle get OperationsStyle;
+  PrintListStyle get FieldsStyle;
+  PrintListStyle get FieldArgumentsStyle;
+  PrintItemStyle get ArgumentValueStyle;
+}
+
+class _GraphQLPrinterIndentedContext extends PrintContext implements GraphQLPrinterContext {
 
   final PrintListStyle OperationsStyle = PrintListStyle(
       newline: true
@@ -77,7 +82,7 @@ class _GraphQLPrinterIndentedContext extends PrintContext implements _GraphQLPri
 
 }
 
-class _GraphQLPrinterContext extends PrintContext {
+class _GraphQLPrinterCompactContext extends PrintContext implements GraphQLPrinterContext{
 
   final PrintListStyle OperationsStyle = PrintListStyle(
       newline: false,
@@ -102,3 +107,4 @@ class _GraphQLPrinterContext extends PrintContext {
   );
 
 }
+
